27ca80bba795af562ea8bc612039d99e5160bee1
Jade Miller <1jaremil1@gmail.com>
2025-07-23 12:03:52 -0600

    i

---
+++ oauth/routes.js
@@ -0,0 +1,28 @@
+const passport = require('passport');
+
+const routes = require("express").Router();
+
+const path = require('path');
+
+const { User } = require("./connection");
+
+routes.get("/", async (req, res) => {
+  res.send(`<a href="/auth/signin">Login with Google</a>`);
+});
+
+routes.get("/auth", passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+routes.get('/auth/',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  (req, res) => {
+    res.redirect('/index');
+});
+
+routes.get('/index', (req, res) => {
+  if (!req.isAuthenticated()) {
+    return res.redirect('/');
+  }
+  res.redirect('./index.html');
+});
+
+module.exports = routes;
---
+++ server.js
@@ -0,0 +1,50 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const passport = require('passport');
+const session = require('express-session');
+const express = require('express');
+const path = require('path');
+var app = express();
+require('./oauth/google_auth.js');
+
+app.use('/styles', express.static(path.join(__dirname, 'styles')));
+
+const MongoStore = require('connect-mongo');
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI })
+}));
+
+app.use(passport.initialize());
+app.use(passport.session()); 
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+// app.set('layout', 'layouts/layout');
+app.use(express.static('public'));
+app.use(express.static('styles'));
+
+const projectsRouter = require('./oauth/routes.js')
+app.use('/', projectsRouter);
+
+const multer = require("multer");
+const cors = require("cors");
+
+// const port = process.env.PORT || 3000;
+
+const upload = multer({ dest: "uploads/" });
+
+app.use(cors());
+
+app.use(express.static("public"));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000')
+});
