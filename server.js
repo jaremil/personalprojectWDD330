ebe7dd1a13ffd36622b9823093898942e978de57
Jade Miller <1jaremil1@gmail.com>
2025-07-23 22:03:50 -0600

    m

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const passport = require('passport');
 const session = require('express-session');
 
-require('./auth/google')(passport); 
+require('/auth/google')(passport); 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
